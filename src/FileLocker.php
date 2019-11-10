d5754868db59613e387fad1c5134d24ba78a6888
jwillp <jwilliam.perreault@morebec.com>
2019-11-10 19:18:11 +0100

    Add workaround code for semaphore extension on windows

--- src/FileLocker.php
+++ src/FileLocker.php
@@ -7,6 +7,18 @@ use Symfony\Component\Lock\Factory;
 use Symfony\Component\Lock\LockInterface;
 use Symfony\Component\Lock\Store\SemaphoreStore;
 
+if ( !function_exists('sem_get') ) {
+    function sem_get($key) { return fopen(__FILE__.'.sem.'.$key, 'w+'); }
+}
+
+if ( !function_exists('sem_acquire') ) {
+    function sem_acquire($sem_id) { return flock($sem_id, LOCK_EX); }
+}
+
+if ( !function_exists('sem_release') ) {
+    function sem_release($sem_id) { return flock($sem_id, LOCK_UN); }
+}
+
 /**
   * FileLocker 
   */
