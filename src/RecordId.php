2d0ce5833609803bc82e76b02d035eba46c26b57
jwillp <jwilliam.perreault@morebec.com>
2019-11-25 19:10:15 +0100

    Remove inheritance to UuidIdentifier on RecordId

--- src/RecordId.php
+++ src/RecordId.php
@@ -1,13 +1,25 @@
-<?php 
+<?php
 
 namespace Morebec\YDB;
 
+use Assert\Assertion;
 use Morebec\ValueObjects\Identity\UuidIdentifier;
+use Morebec\ValueObjects\StringBasedValueObject;
 use Morebec\YDB\Database\RecordIdInterface;
 
 /**
  * RecordId
  */
-class RecordId extends UuidIdentifier implements RecordIdInterface
-{    
+class RecordId extends StringBasedValueObject implements RecordIdInterface
+{
+    function __construct(string $identifier)
+    {
+        Assertion::notBlank($identifier, 'A record id cannot be blank.');
+        parent::__construct($identifier); 
+    }
+
+    public static function generate(): RecordId
+    {
+        return new static(UuidIdentifier::generate());
+    }
 }
